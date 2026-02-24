643e38c46c2e68b553045ed1d045387efcb26ed0
KommoTeam-Yaroslav-Mizin <ymizin@team.amocrm.com>
2026-02-24 18:35:53 +0300

    ui update

--- src/shared/ui/Tooltip/ui/Tooltip.props.ts
+++ src/shared/ui/Tooltip/ui/Tooltip.props.ts
@@ -1,8 +1,11 @@
-import { type ReactNode } from 'react';
+import { type ReactNode, type CSSProperties } from 'react';
 
 export interface TooltipProps {
   children: ReactNode;
   content?: string;
   position?: 'top' | 'bottom' | 'left' | 'right';
   isVisible: boolean;
+  className?: string;
+  tooltipClassName?: string;
+  style?: CSSProperties;
 }
--- src/shared/ui/Tooltip/ui/Tooltip.tsx
+++ src/shared/ui/Tooltip/ui/Tooltip.tsx
@@ -5,15 +5,23 @@ import { type TooltipProps } from './Tooltip.props';
 import styles from './Tooltip.module.css';
 
 export const Tooltip: FC<TooltipProps> = (props) => {
-  const { children, content, position = 'right', isVisible = false } = props;
+  const {
+    children,
+    content,
+    position = 'right',
+    isVisible = false,
+    className = '',
+    tooltipClassName = '',
+    style,
+  } = props;
 
   return (
-    <div className={styles['tooltip-wrapper']}>
+    <div className={`${styles['tooltip-wrapper']} ${className}`} style={style}>
       {children}
 
       {isVisible && content && (
         <div
-          className={`${styles['tooltip']} ${styles[`tooltip--${position}`]}`}
+          className={`${styles['tooltip']} ${styles[`tooltip--${position}`]} ${tooltipClassName}`}
         >
           {content}
         </div>
