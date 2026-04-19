6f84d4ce1c7763ec712e09303fa6019d348538cb
Yaroslav <jaroslavmizin@yandex.ru>
2026-04-19 15:41:57 +0300

    модальное окно

---
+++ src/shared/ui/Modal/ui/Modal.props.ts
@@ -0,0 +1,24 @@
+import { type ReactNode } from 'react';
+
+export interface ModalProps {
+  /**
+   * Is modal open
+   */
+  isOpen: boolean;
+  /**
+   * Callback for close modal
+   */
+  onClose: () => void;
+  /**
+   * Modal content
+   */
+  children: ReactNode;
+  /**
+   * Custom class
+   */
+  className?: string;
+  /**
+   * Custom class for modal content
+   */
+  contentClassName?: string;
+}
---
+++ src/shared/ui/Modal/ui/Modal.tsx
@@ -0,0 +1,32 @@
+import React, { type FC } from 'react';
+
+import type { ModalProps } from './Modal.props';
+
+import styles from './Modal.module.css';
+
+export const Modal: FC<ModalProps> = (props) => {
+  const {
+    className = '',
+    contentClassName = '',
+    children,
+    isOpen,
+    onClose,
+  } = props;
+
+  return (
+    <div className={styles.overlay}>
+      <div
+        className={`${styles.modal} ${className}`}
+        style={{ display: isOpen ? 'flex' : 'none' }}
+      >
+        <div className={styles['icon-wrapper']} onClick={onClose}>
+          <div className={styles['close-icon']}></div>
+        </div>
+
+        <div className={`${styles.content} ${contentClassName}`}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
