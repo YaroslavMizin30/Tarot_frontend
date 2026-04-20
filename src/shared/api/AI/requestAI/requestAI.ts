54227e1ace070df842f6a1eee3f96b3a83b905fc
Yaroslav <jaroslavmizin@yandex.ru>
2026-04-20 20:21:15 +0300

    перенес ии-запрос на бэкенд

--- src/shared/api/AI/requestAI/requestAI.ts
+++ src/shared/api/AI/requestAI/requestAI.ts
@@ -4,19 +4,13 @@ interface Prompt {
 }
 
 export const requestAi = async (prompts: Prompt[]) => {
-  const response = await fetch('https://routerai.ru/api/v1/chat/completions', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer sk-VJYm9TLvOSZY--3UOkAr0JI9AbNRXj8S',
+  const response = await fetch(
+    'https://hadjujaanfwgepckluqy.supabase.co/functions/v1/ai-request',
+    {
+      method: 'POST',
+      body: JSON.stringify({ prompt: prompts }),
     },
-    body: JSON.stringify({
-      model: 'arcee-ai/trinity-large-thinking',
-      messages: prompts,
-    }),
-  });
+  );
 
-  const body = await response.json();
-
-  return body.choices[0].message.content;
+  return response.json();
 };
