72f4ba35ad0b6009447df4fcddd084c3c416aed9
Yaroslav <jaroslavmizin@yandex.ru>
2026-04-21 20:27:17 +0300

    правки конфига раскладов

--- src/features/TarotSpread/config/spreads/three.ts
+++ src/features/TarotSpread/config/spreads/three.ts
@@ -1,8 +1,8 @@
 export const ThreeCardSpread = {
   cards: [
-    { right: '52px', top: '20px', index: 1, title: 'Past' },
-    { left: 'calc(50% - 43px)', top: '20px', index: 2, title: 'Present' },
-    { left: '52px', top: '20px', index: 3, title: 'Future' },
+    { right: '52px', top: '20px', index: 1 },
+    { left: 'calc(50% - 43px)', top: '20px', index: 2 },
+    { left: '52px', top: '20px', index: 3 },
   ],
   button: { right: 'calc(50% + 100px)', top: '192px' },
 };
