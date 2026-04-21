28e3b1273ef4b55101a71963d19d87d5a7e43c79
Yaroslav <jaroslavmizin@yandex.ru>
2026-04-21 23:42:54 +0300

    фолбэк на страницу расклада

--- src/pages/Spread/ui/SpreadPage.tsx
+++ src/pages/Spread/ui/SpreadPage.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
-import { useLocation } from 'react-router';
+import { Link, useLocation } from 'react-router';
 
 import useLocales from '@/shared/hooks/useLocales';
 import TextContainer from '@/shared/ui/TextContainer';
 import TRANSLATIONS_EN from '@/shared/locales/en/history';
 import TRANSLATIONS_RU from '@/shared/locales/ru/history';
 import RatingInput from '@/shared/ui/RatingInput';
+import Arrow from '@/shared/assets/svg/common/deck-arrow.svg?react';
 
 import { type Spread, updateSpread } from '@/entities/Spread';
 import TarotCard from '@/entities/TarotCard';
@@ -82,6 +83,10 @@ export const SpreadPage = () => {
       />
 
       <RatingInput value={rate ?? 0} onChange={handleRatingInputChange} />
+
+      <Link className={styles.link} to={'/history'}>
+        <Arrow />
+      </Link>
     </div>
   );
 };
