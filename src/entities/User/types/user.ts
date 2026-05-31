3de49f9ba441edebce78e5ed095396b161ef8a18
Yaroslav <jaroslavmizin@yandex.ru>
2026-05-31 16:55:49 +0300

    правка типа пользователя

--- src/entities/User/types/user.ts
+++ src/entities/User/types/user.ts
@@ -1,37 +1,21 @@
 import { type Sign } from '@/shared/ui/Zodiac';
 
 export interface GetUserResponse {
-  activity_preference: string;
-  communication_type: string | null;
-  day_spending: string;
-  decision_making: string;
-  goals: string;
   id: number;
-  energy_type: string;
-  last_horoscope: number | null;
-  relationship_preference: string;
-  resort_preference: string;
   sign: Sign;
-  situation: string;
-  stress_reaction: string;
   user_name: string;
-  way_of_life: string;
+  birth_date: string;
+  birth_time?: string;
+  birth_place: string;
+  natal_chart: string;
 }
 
 export interface User {
-  activityPreference: string;
-  communicationType: string | null;
-  daySpending: string;
-  decisionMaking: string;
-  goals: string;
   id: number;
-  energyType: string;
-  lastHoroscope: number | null;
-  relationshipPreference: string;
-  resortPreference: string;
   sign: Sign;
-  situation: string;
-  stressReaction: string;
   userName: string;
-  wayOfLife: string;
+  birthDate: string;
+  birthTime?: string;
+  birthPlace: string;
+  natalChart: string;
 }
