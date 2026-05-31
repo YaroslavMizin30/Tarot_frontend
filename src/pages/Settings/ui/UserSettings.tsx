d30bf7c8bacac9b4144a8426b9b368e42e9814a0
Yaroslav <jaroslavmizin@yandex.ru>
2026-05-31 22:15:00 +0300

    обновил страницу настроек

--- src/pages/Settings/ui/UserSettings.tsx
+++ src/pages/Settings/ui/UserSettings.tsx
@@ -1,27 +1,19 @@
 import { useState } from 'react';
-import snakeize from 'snakeize';
 
-import { useUserData, type User } from '@/entities/User';
+import { useUserData } from '@/entities/User';
+import NatalChart from '@/entities/NatalChart';
 
-import useLocales from '@/shared/hooks/useLocales';
-import Button from '@/shared/ui/Button';
 import Arrow from '@/shared/assets/svg/common/deck-arrow.svg';
-
-import { HIDDEN_FIELDS } from '../config/fields';
-import { OPTIONS } from '../config/options';
 
 import styles from './SettingsPage.module.css';
 
 const UserSettings = (props: { onBackButtonClick: () => void }) => {
   const { onBackButtonClick } = props;
 
-  const { userData, updateUserData, isLoading } = useUserData();
+  const { userData } = useUserData();
 
   const [options, setOptions] = useState<string[]>([]);
   const [title, setTitle] = useState<string>('About you');
-  const [currentUpdate, setCurrentUpdate] = useState<string>('');
-
-  const { i18n } = useLocales();
 
   const handleBackButtonClick = () => {
     if (options.length) {
@@ -39,69 +31,16 @@ const UserSettings = (props: { onBackButtonClick: () => void }) => {
     return null;
   }
 
-  const getContent = () => {
-    if (options.length) {
-      return options.map((option) => {
-        const handleOptionClick = async () => {
-          setCurrentUpdate(option);
-
-          await updateUserData(
-            String(userData?.id),
-            snakeize({
-              ...userData,
-              [title]: i18n(option),
-            }),
-          );
-        };
-
-        return (
-          <Button
-            isActive={
-              i18n(option) === userData?.[title as keyof typeof userData]
-            }
-            isLoading={currentUpdate === option && isLoading}
-            onClick={handleOptionClick}
-            className={styles.button}
-            key={option}
-          >
-            {i18n(option)}
-          </Button>
-        );
-      });
-    }
-
-    return (
-      <>
-        {Object.keys(userData).map((key) => {
-          if (HIDDEN_FIELDS.includes(key) || !userData[key as keyof User]) {
-            return null;
-          }
-
-          const handleOptionButtonClick = () => {
-            setOptions(OPTIONS[key as keyof User]);
-
-            setTitle(key);
-          };
-
-          return (
-            <div key={key} className={styles.option}>
-              <p>{i18n(key)}</p>
-
-              <Button onClick={handleOptionButtonClick}>
-                {userData[key as keyof typeof userData]}
-              </Button>
-            </div>
-          );
-        })}
-      </>
-    );
-  };
-
   return (
     <>
-      <h4 className={styles.title}>{i18n(title)}</h4>
+      <h3>{title}</h3>
 
-      <div className={`${styles.section} custom-scrollbar`}>{getContent()}</div>
+      <NatalChart
+        {...userData}
+        zodiacSign={userData.sign}
+        name={userData.userName}
+        chartDescription={userData.natalChart}
+      />
 
       <Arrow
         width={30}
