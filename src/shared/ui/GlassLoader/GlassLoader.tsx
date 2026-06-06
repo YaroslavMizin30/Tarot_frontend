dbe1caf52e095f538c5f29fc5b6a585473b44668
Yaroslav <jaroslavmizin@yandex.ru>
2026-06-06 21:12:10 +0300

    лодер стекляшка

---
+++ src/shared/ui/GlassLoader/GlassLoader.tsx
@@ -0,0 +1,9 @@
+import styles from './Glassloader.module.css';
+
+export const GlassLoader = () => {
+  return (
+    <div className={styles['glass-loader']}>
+      {/* Опционально: можно добавить иконку или текст по центру */}
+    </div>
+  );
+};
