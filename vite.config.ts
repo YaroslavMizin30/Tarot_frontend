96bf74c7746fa8381a1fa3946c5cdf40c72e3dc6
Yaroslav <jaroslavmizin@yandex.ru>
2026-06-06 21:34:57 +0300

    бандл аналайзер

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import dts from 'vite-plugin-dts';
+import { analyzer } from 'vite-bundle-analyzer';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({include: '**/*.svg'}), dts()],
+  plugins: [
+    react(),
+    svgr({ include: '**/*.svg' }),
+    dts(),
+    analyzer({ analyzerMode: 'server' }),
+  ],
   resolve: {
     alias: {
       '@/app': path.resolve(__dirname, './src/app'),
