192fbd414aafe542f5e6d5a005eeaa52891b8357
Yaroslav <jaroslavmizin@yandex.ru>
2026-06-07 16:50:20 +0300

    добавил ArrowButton

---
+++ src/shared/ui/ArrowButton/index.ts
@@ -0,0 +1,1 @@
+export { ArrowButton as default } from './ui/ArrowButton';
---
+++ src/shared/ui/ArrowButton/ui/ArrowButton.tsx
@@ -0,0 +1,17 @@
+import { type FC, type ButtonHTMLAttributes } from 'react';
+
+import Arrow from '@/shared/assets/svg/common/deck-arrow.svg';
+
+import styles from './ArrowButton.module.css';
+
+export const ArrowButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = (
+  props,
+) => {
+  const { className } = props;
+
+  return (
+    <button {...props} className={`${styles.button} ${className}`}>
+      <Arrow width={30} height={30} />
+    </button>
+  );
+};
