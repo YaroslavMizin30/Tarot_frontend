92e4a4cc9ada7ca7078d65390afef9d5728ff826
Yaroslav <jaroslavmizin@yandex.ru>
2026-06-07 18:56:59 +0300

    багфикс использования тултипа

--- src/shared/ui/RadioSheet/ui/RadioSheet.tsx
+++ src/shared/ui/RadioSheet/ui/RadioSheet.tsx
@@ -1,4 +1,4 @@
-import { useState, type ChangeEvent, type FC, type MouseEvent } from 'react';
+import { type ChangeEvent, type FC } from 'react';
 
 import { Tooltip } from '../../Tooltip/ui/Tooltip';
 
@@ -13,29 +13,13 @@ export const RadioSheet: FC<RadioSheetProps> = (props) => {
     onChange(e.target.value);
   };
 
-  const [visibleElement, setVisibleElement] = useState('');
-
-  const handleMouseEnter = (event: MouseEvent<HTMLDivElement>) => {
-    setVisibleElement(event.currentTarget.title);
-  };
-
-  const handleMouseLeave = () => {
-    setVisibleElement('');
-  };
-
   return (
     <div className={`${styles.container} ${className}`}>
       <h3 className={styles.title}>{title}</h3>
 
       <div className={styles.list}>
         {items.map((item) => (
-          <div
-            className={styles.item}
-            title={item.id}
-            key={item.id}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
+          <div className={styles.item} title={item.id} key={item.id}>
             <label
               htmlFor={item.id}
               className={`${styles.label} ${item.id === value && styles['label-selected']}`}
