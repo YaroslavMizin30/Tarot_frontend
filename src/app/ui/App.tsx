313ca1b29e52c6a2988f6262794e19d60af829b4
Yaroslav <jaroslavmizin@yandex.ru>
2026-06-19 10:48:06 +0300

    перенес вызов в useEffect

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -6,18 +6,19 @@ import router from '../router';
 import store from '../store';
 
 import useTelegram from '@/shared/hooks/useTelegram';
+import { useEffect } from 'react';
 
 export function App() {
   const { prepareApp, isMobile, hideHeaderButtons, expandApp } = useTelegram();
 
-  document.addEventListener('DOMContentLoaded', () => {
+  useEffect(() => {
     prepareApp();
 
     if (isMobile) {
       hideHeaderButtons();
       expandApp();
     }
-  });
+  }, []);
 
   return (
     <Provider store={store}>
