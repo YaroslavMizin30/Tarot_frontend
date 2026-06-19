d25850b5665a94c1a85efbab56f1b6778cc753a2
Yaroslav <jaroslavmizin@yandex.ru>
2026-06-19 11:09:48 +0300

    отслеживание скрипта

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -5,21 +5,7 @@ import { Provider } from 'react-redux';
 import router from '../router';
 import store from '../store';
 
-import useTelegram from '@/shared/hooks/useTelegram';
-import { useEffect } from 'react';
-
 export function App() {
-  const { prepareApp, isMobile, hideHeaderButtons, expandApp } = useTelegram();
-
-  useEffect(() => {
-    prepareApp();
-
-    if (isMobile) {
-      hideHeaderButtons();
-      expandApp();
-    }
-  }, []);
-
   return (
     <Provider store={store}>
       <RouterProvider router={router} />
--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,15 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from '@/app';
 
+document.getElementById('telegram')?.addEventListener('load', () => {
+  const telegram = window?.Telegram?.WebApp;
+
+  telegram?.ready();
+
+  if (telegram?.platform === 'ios' || telegram?.platform === 'android') {
+    telegram?.SettingsButton?.hide();
+    telegram?.expand();
+  }
+});
+
 createRoot(document.getElementById('root')!).render(<App />);
