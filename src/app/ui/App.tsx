fa788c0c4ac42a3793a569405f952285c08c6350
Yaroslav <jaroslavmizin@yandex.ru>
2026-06-19 09:23:42 +0300

    эксперименты с хедером

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -7,6 +7,8 @@ import store from '../store';
 
 export function App() {
   window.Telegram?.WebApp?.setHeaderColor?.('#ffdb88');
+  window.Telegram?.WebApp?.BackButton?.hide();
+  window.Telegram?.WebApp?.MainButton?.hide();
 
   return (
     <Provider store={store}>
