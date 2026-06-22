05fcf38d4fc24274e85e212025f72a41153d5b2d
Yaroslav <jaroslavmizin@yandex.ru>
2026-06-22 18:56:34 +0300

    свеча с анимацией

---
+++ src/shared/ui/Torch/ui/Torch.tsx
@@ -0,0 +1,24 @@
+import { type FC } from 'react';
+
+import type { TorchProps } from './Torch.props';
+
+import styles from './Torch.module.css';
+
+const Torch: FC<TorchProps> = (props) => {
+  const { style = {}, className } = props;
+
+  return (
+    <div className={`${styles.torch} ${className}`} style={style}>
+      <div className={styles.flame}>
+        <div className={styles.red}></div>
+        <div className={styles.yellow}></div>
+        <div className={styles.blue}></div>
+        <div className={styles.wick}></div>
+      </div>
+      <div className={styles.stick}></div>
+      <div className={styles.bottom}></div>
+    </div>
+  );
+};
+
+export default Torch;
