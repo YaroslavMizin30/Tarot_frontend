268d5df71129a3736c817a2d674e686268823e03
Yaroslav <jaroslavmizin@yandex.ru>
2026-06-24 18:43:31 +0300

    переводы для страницы астрологии

---
+++ src/shared/locales/en/astrology.ts
@@ -0,0 +1,6 @@
+const TRANSLATIONS_EN: Record<string, string> = {
+  'Natal chart': 'Natal chart',
+  'Horoscopes': 'Horoscopes',
+};
+
+export default TRANSLATIONS_EN;
---
+++ src/shared/locales/ru/astrology.ts
@@ -0,0 +1,6 @@
+const TRANSLATIONS_RU: Record<string, string> = {
+  'Natal chart': 'Натальная карта',
+  'Horoscopes': 'Гороскопы',
+};
+
+export default TRANSLATIONS_RU;
