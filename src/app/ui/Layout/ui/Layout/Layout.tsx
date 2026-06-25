e5bfa6691218469e423ea6b71cfe4f81e9ed5307
Yaroslav <jaroslavmizin@yandex.ru>
2026-06-25 06:30:38 +0300

    логика покраски футера

--- src/app/ui/Layout/ui/Layout/Layout.tsx
+++ src/app/ui/Layout/ui/Layout/Layout.tsx
@@ -46,16 +46,14 @@ export const Layout = () => {
   }, [locale]);
 
   useEffect(() => {
-    if (isAuthenticating) {
+    if (theme) {
+      document.documentElement.setAttribute('data-theme', theme);
+      window.Telegram?.WebApp?.setHeaderColor(themeConfig[theme].header);
+      window.Telegram?.WebApp?.setBottomBarColor(themeConfig[theme].footer);
+    } else {
       window.Telegram?.WebApp?.setHeaderColor('#323232');
       window.Telegram?.WebApp?.setBottomBarColor('#323232');
-
-      return;
     }
-
-    document.documentElement.setAttribute('data-theme', theme);
-    window.Telegram?.WebApp?.setHeaderColor(themeConfig[theme].header);
-    window.Telegram?.WebApp?.setBottomBarColor(themeConfig[theme].footer);
   }, [theme]);
 
   return (
