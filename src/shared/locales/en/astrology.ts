1bd8aff2ab52feff0f05f8c9d81857ae2bdf566f
Yaroslav <jaroslavmizin@yandex.ru>
2026-06-26 21:43:19 +0300

    ленги

--- src/shared/locales/en/astrology.ts
+++ src/shared/locales/en/astrology.ts
@@ -1,6 +1,42 @@
 const TRANSLATIONS_EN: Record<string, string> = {
   'Natal chart': 'Natal chart',
   'Horoscopes': 'Horoscopes',
+
+  'In sign': 'In sign',
+  'Phase': 'Phase',
+  'Next phases': 'Next phases',
+
+  'New Moon': 'New Moon',
+  'Waxing Crescent': 'Waxing Crescent',
+  'First Quarter': 'First Quarter',
+  'Waxing Gibbous': 'Waxing Gibbous',
+  'Full Moon': 'Full Moon',
+  'Waning Gibbous': 'Waning Gibbous',
+  'Last Quarter': 'Last Quarter',
+  'Waning Crescent': 'Waning Crescent',
+
+  'newMoon': 'newMoon',
+  'waxingCrescent': 'waxingCrescent',
+  'firstQuarter': 'firstQuarter',
+  'waxingGibbous': 'waxingGibbous',
+  'fullMoon': 'fullMoon',
+  'waningGibbous': 'waningGibbous',
+  'lastQuarter': 'lastQuarter',
+  'waningCrescent': 'waningCrescent',
+
+  // Знаки зодиака
+  'Aries': 'Aries',
+  'Taurus': 'Taurus',
+  'Gemini': 'Gemini',
+  'Cancer': 'Cancer',
+  'Leo': 'Leo',
+  'Virgo': 'Virgo',
+  'Libra': 'Libra',
+  'Scorpio': 'Scorpio',
+  'Sagittarius': 'Sagittarius',
+  'Capricorn': 'Capricorn',
+  'Aquarius': 'Aquarius',
+  'Pisces': 'Pisces',
 };
 
 export default TRANSLATIONS_EN;
--- src/shared/locales/ru/astrology.ts
+++ src/shared/locales/ru/astrology.ts
@@ -1,6 +1,42 @@
 const TRANSLATIONS_RU: Record<string, string> = {
   'Natal chart': 'Натальная карта',
   'Horoscopes': 'Гороскопы',
+
+  'In sign': 'В знаке',
+  'Phase': 'Фаза',
+  'Next phases': 'Следующие фазы',
+
+  'New Moon': 'Новолуние',
+  'Waxing Crescent': 'Растущий серп',
+  'First Quarter': 'Первая четверть',
+  'Waxing Gibbous': 'Восходящая луна',
+  'Full Moon': 'Полнолуние',
+  'Waning Gibbous': 'Убывающая луна',
+  'Last Quarter': 'Последняя четверть',
+  'Waning Crescent': 'Убывающий серп',
+
+  'newMoon': 'Новолуние',
+  'waxingCrescent': 'Растущий серп',
+  'firstQuarter': 'Первая четверть',
+  'waxingGibbous': 'Восходящая луна',
+  'fullMoon': 'Полнолуние',
+  'waningGibbous': 'Убывающая луна',
+  'lastQuarter': 'Последняя четверть',
+  'waningCrescent': 'Убывающий серп',
+
+  // Знаки зодиака
+  'Aries': 'Овен',
+  'Taurus': 'Телец',
+  'Gemini': 'Близнецы',
+  'Cancer': 'Рак',
+  'Leo': 'Лев',
+  'Virgo': 'Дева',
+  'Libra': 'Весы',
+  'Scorpio': 'Скорпион',
+  'Sagittarius': 'Стрелец',
+  'Capricorn': 'Козерог',
+  'Aquarius': 'Водолей',
+  'Pisces': 'Рыбы',
 };
 
 export default TRANSLATIONS_RU;
