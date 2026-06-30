9cf5c90c973a291ae9095051f0e8e730dca8a88d
Yaroslav <jaroslavmizin@yandex.ru>
2026-06-30 23:28:48 +0300

    серпы

--- src/pages/Forecast/ui/Moon/Moon.tsx
+++ src/pages/Forecast/ui/Moon/Moon.tsx
@@ -3,12 +3,18 @@ import type { MoonProps } from './Moon.props';
 import styles from './Moon.module.css';
 
 const Moon = (props: MoonProps) => {
-  const { phase, size, style } = props;
+  const { phase, size, style = {} } = props;
 
   return (
     <div
       className={`${styles.moon} ${size === 's' ? styles['moon-small'] : ''}`}
-      style={style}
+      style={{
+        ...style,
+        overflow:
+          phase === 'waxingcrescent' || phase === 'waningcrescent'
+            ? 'hidden'
+            : undefined,
+      }}
     >
       <img
         className={styles.image}
